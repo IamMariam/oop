d9efd66492990fc098b2514f848ace92f5adbdf4
Mariam Mourad <134807503+IamMariam@users.noreply.github.com>
2024-01-24 17:41:20 +0300

    Add files via upload

---
+++ Milestonecpp.cpp
@@ -0,0 +1,36 @@
+//#include"Admin.h"
+//#include <windows.h>
+//#include <cstdlib>
+//#include <ctime>
+//#include <fstream>
+//int passed = 0, failed = 0;
+//int Student::ID = 0;
+//int Instructor::ID = 0;
+//int Instructor::NOQTD = 5;
+//int Question::total = 0;
+//string stName;
+//int Student::StdWhoPart = 0;
+//
+//static vector<string> split(string s, char delim) {
+//	vector<string> result;
+//	stringstream ss(s);
+//	string item;
+//	while (getline(ss, item, delim)) {
+//		result.push_back(item);
+//	}
+//	return result;
+//}
+//
+//
+////************************************************************
+//
+////************************************************************
+//
+////************************************************************
+//
+//	//make grades file name odf student and his mark 
+//	// sum+=v[1] get from grades 
+//	// make "users file " only store username and password, type 1,2
+//	// student file store first name ,last name ,major ,email (create profile )
+//	// same for instructor 
+//
